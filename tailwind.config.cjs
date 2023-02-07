a6bc8db40087ed33a246cd91029b669f92c33d97
Henrique Almeida <henriquealmeida@MacBook-Pro-de-Henrique.local>
2023-02-07 01:50:48 -0300

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,46 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      clip: {
+        none: 'rect(0 0 0 0)',
+      },
+      backgroundImage: {
+        bgImage: "url('/src/assets/BG.png')",
+      },
+      colors: {
+        "dark-purple": "#081A51",
+        "light-white": "rgba(255,255,255,0.18)",
+      },
+      gridTemplateAreas: {
+        clientHydrometerDetails: [
+          "title title title title title",
+          "empresa empresa fantasia cnpj cnpj",
+          "responsavel telefone email endereco endereco",
+          "cidade estado cep serie serie",
+          "nome latitude longitude vazao montadoem",
+        ],
+        clientDetails: [
+          "title title title title title title",
+          "empresa empresa fantasia fantasia cnpj cnpj",
+          "endereco endereco endereco endereco endereco endereco",
+          "responsavel responsavel responsavel responsavel responsavel responsavel",
+          "telefone telefone telefone email email email",
+          "cidade cidade estado estado cep cep",
+          "cadastradoEm cadastradoEm cadastradoEm ultimaAtualizacao ultimaAtualizacao ultimaAtualizacao",
+        ],
+      },
+      gridTemplateColumns: {
+        clientDetails: "1fr 1fr 1fr 1fr 1fr",
+      },
+      gridTemplateRows: {
+        clientDetails: "",
+      },
+    },
+  },
+  plugins: [require("@savvywombat/tailwindcss-grid-areas")],
+  variants: {
+    gridTemplateAreas: ["responsive"],
+  },
+};
