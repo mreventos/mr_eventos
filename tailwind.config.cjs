bc948c533209b04820d7e0350985f23a9e9a3b76
Henrique Almeida <henrique.almeida.ads@gmail.com>
2026-01-09 16:38:25 -0300

    hotfix: corrigindo dependencias

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -13,34 +13,7 @@ module.exports = {
         "dark-purple": "#081A51",
         "light-white": "rgba(255,255,255,0.18)",
       },
-      gridTemplateAreas: {
-        clientHydrometerDetails: [
-          "title title title title title",
-          "empresa empresa fantasia cnpj cnpj",
-          "responsavel telefone email endereco endereco",
-          "cidade estado cep serie serie",
-          "nome latitude longitude vazao montadoem",
-        ],
-        clientDetails: [
-          "title title title title title title",
-          "empresa empresa fantasia fantasia cnpj cnpj",
-          "endereco endereco endereco endereco endereco endereco",
-          "responsavel responsavel responsavel responsavel responsavel responsavel",
-          "telefone telefone telefone email email email",
-          "cidade cidade estado estado cep cep",
-          "cadastradoEm cadastradoEm cadastradoEm ultimaAtualizacao ultimaAtualizacao ultimaAtualizacao",
-        ],
-      },
-      gridTemplateColumns: {
-        clientDetails: "1fr 1fr 1fr 1fr 1fr",
-      },
-      gridTemplateRows: {
-        clientDetails: "",
-      },
     },
   },
-  plugins: [require("@savvywombat/tailwindcss-grid-areas")],
-  variants: {
-    gridTemplateAreas: ["responsive"],
-  },
+  plugins: [],
 };
